be57ab1ca62dc9c5cf842d96a5e9e39ef87c95c2
richardsi21 <iparraguirrerichard21@gmail.com>
2025-06-13 19:56:52 -0400

    Update project.java

--- project.java
+++ project.java
@@ -21,8 +21,30 @@ public class monitor {
         }
     }
     private static void processURL(String urlStr) {
-        try{
-        }catch{
+        try {
+            System.out.println("URL: " + urlStr);
+            URL url = new URL(urlStr);
+            String protocol = url.getProtocol();
+            String host = url.getHost();
+            int port = url.getPort() == -1 ? (protocol.equals("https") ? 443 : 80) : url.getPort();
+            String path = url.getPath().isEmpty() ? "/" : url.getPath();
+
+            Socket sockets;
+            if (protocol.equals("https")) {
+                SSLSocketFactory sslFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+                socket = sslFactory.createSocket(host, port);
+            } else if (protocol.equals("http")) {
+                socket = new Socket();
+                socket.connect(new InetSocketAddress(host, port), 5000);
+            } else {
+                System.out.println("Status: Unsupported Protocol");
+                return;
+            }
+
+            
+            
+        }catch (Exception e) {
+            System.out.pritnln("Status: Network Error");
             }
     }
 
